b1375aac9618a13ebe1a89ef7c194443dc8df810
Matthew Tran <matthewtran0122@gmail.com>
2024-10-19 17:52:03 -0700

    wrapped the data in the mocha.it call

--- src/server/test/web/readingsBarGroupFlow.js
+++ src/server/test/web/readingsBarGroupFlow.js
@@ -113,57 +113,60 @@ mocha.describe('readings API', () => {
                 });
 
                 // Add BG16 here
-                const unitData = [
-                    {
-                        // u14
-                        name: "Thing_36",
-                        identifier: "",
-                        unitRepresent: Unit.unitRepresentType.FLOW,
-                        secInRate: 36,
-                        typeOfUnit: Unit.unitType.METER,
-                        suffix: "",
-                        displayable: Unit.displayableType.NONE,
-                        preferredDisplay: false,
-                        note: "special unit"
-                    },
-                    {
-                        // u15
-                        name: "thing unit",
-                        identifier: "",
-                        unitRepresent: Unit.unitRepresentType.FLOW,
-                        secInRate: 3600,
-                        typeOfUnit: Unit.unitType.UNIT,
-                        suffix: "",
-                        displayable: Unit.displayableType.ALL,
-                        preferredDisplay: false,
-                        note: "special unit"
-                     }
-                ];
-                const conversionData = [
-                    {
-                        // c15
-                        sourceName: "Thing_36",
-                        destinationName: "thing unit",
-                        bidirectional: false,
-                        slope: 1,
-                        intercept: 0,
-                        note: "Thing_36 → thing unit"
-                     }
-                ];
-                const meterData = [
-                    {
-                        name: 'Thing_36 thing unit',
-                        unit: 'Thing_36',
-                        defaultGraphicUnit: 'thing unit',
-                        displayable: true,
-                        gps: undefined,
-                        note: 'special meter',
-                        file: 'test/web/readingsData/readings_ri_15_days_75.csv',
-                        deleteFile: false,
-                        readingFrequency: '15 minutes',
-                        id: METER_ID
-                    }
-                ];
+                mocha.it('BG16: should have daily points for 15 + 20 minute reading intervals and flow units with +-inf start/end time & thing as thing where rate is 36', async () => {
+                    const unitData = [
+                        {
+                            // u14
+                            name: "Thing_36",
+                            identifier: "",
+                            unitRepresent: Unit.unitRepresentType.FLOW,
+                            secInRate: 36,
+                            typeOfUnit: Unit.unitType.METER,
+                            suffix: "",
+                            displayable: Unit.displayableType.NONE,
+                            preferredDisplay: false,
+                            note: "special unit"
+                        },
+                        {
+                            // u15
+                            name: "thing unit",
+                            identifier: "",
+                            unitRepresent: Unit.unitRepresentType.FLOW,
+                            secInRate: 3600,
+                            typeOfUnit: Unit.unitType.UNIT,
+                            suffix: "",
+                            displayable: Unit.displayableType.ALL,
+                            preferredDisplay: false,
+                            note: "special unit"
+                         }
+                    ];
+                    const conversionData = [
+                        {
+                            // c15
+                            sourceName: "Thing_36",
+                            destinationName: "thing unit",
+                            bidirectional: false,
+                            slope: 1,
+                            intercept: 0,
+                            note: "Thing_36 → thing unit"
+                         }
+                    ];
+                    const meterData = [
+                        {
+                            name: 'Thing_36 thing unit',
+                            unit: 'Thing_36',
+                            defaultGraphicUnit: 'thing unit',
+                            displayable: true,
+                            gps: undefined,
+                            note: 'special meter',
+                            file: 'test/web/readingsData/readings_ri_15_days_75.csv',
+                            deleteFile: false,
+                            readingFrequency: '15 minutes',
+                            id: METER_ID
+                        }
+                    ];
+                });
+                
 
             });
         });
