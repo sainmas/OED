261f536725f9e210bc0f7833e196a83d9dbaf1ea
Steven Huss-Lederman <huss@beloit.edu>
2024-10-29 10:15:43 -0500

    have file consistently use tabs

--- src/server/test/web/readingsCompareMeterFlow.js
+++ src/server/test/web/readingsCompareMeterFlow.js
@@ -3,26 +3,26 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 /*
-    This file tests the readings retrieval API compare chart meters.
-    See: https://github.com/OpenEnergyDashboard/DesignDocs/blob/main/testing/testing.md for information.
+	This file tests the readings retrieval API compare chart meters.
+	See: https://github.com/OpenEnergyDashboard/DesignDocs/blob/main/testing/testing.md for information.
 */
 const { chai, mocha, app } = require('../common');
 const {
-    prepareTest,
-    expectCompareToEqualExpected,
-    getUnitId,
-    METER_ID,
-    unitDatakWh,
-    conversionDatakWh,
-    meterDatakWh
+	prepareTest,
+	expectCompareToEqualExpected,
+	getUnitId,
+	METER_ID,
+	unitDatakWh,
+	conversionDatakWh,
+	meterDatakWh
 } = require('../../util/readingsUtils');
 const Unit = require('../../models/Unit');
 
 mocha.describe('readings API', () => {
-    mocha.describe('readings test, test if data returned by API is as expected', () => {
-        mocha.describe('for compare charts', () => {
-            mocha.describe('for meters', () => {
-                // Add C15 here
+	mocha.describe('readings test, test if data returned by API is as expected', () => {
+		mocha.describe('for compare charts', () => {
+			mocha.describe('for meters', () => {
+				// Add C15 here
 				mocha.it('C15: 7 day shift end 2022-10-31 17:00:00 for 15 minute reading intervals and flow units & kW as kW', async () => {
 					const unitData = [
 						{
@@ -90,8 +90,8 @@ mocha.describe('readings API', () => {
 					
 						expectCompareToEqualExpected(res, expected);
 					});
-                // Add C16 here
-            });
-        });
-    });
+				// Add C16 here
+			});
+		});
+	});
 });
