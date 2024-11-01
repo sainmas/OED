de7dd83fcf5376cbb72c2be3240f14a91b8382f9
Austin Keller <29049074+aukeller@users.noreply.github.com>
2024-10-31 18:48:30 -0700

    added units for test case 14

--- src/server/test/web/readingsCompareMeterQuantity.js
+++ src/server/test/web/readingsCompareMeterQuantity.js
@@ -6,87 +6,217 @@
 	This file tests the readings retrieval API compare chart meters.
 	See: https://github.com/OpenEnergyDashboard/DesignDocs/blob/main/testing/testing.md for information.
 */
-const { chai, mocha, app } = require('../common');
-const { prepareTest,
+const { chai, mocha, app } = require("../common");
+const {
+	prepareTest,
 	expectCompareToEqualExpected,
 	getUnitId,
 	METER_ID,
 	unitDatakWh,
 	conversionDatakWh,
-	meterDatakWh } = require('../../util/readingsUtils');
+	meterDatakWh,
+} = require("../../util/readingsUtils");
 
-mocha.describe('readings API', () => {
-	mocha.describe('readings test, test if data returned by API is as expected', () => {
-		mocha.describe('for compare charts', () => {
-			mocha.describe('for meters', () => {
-				// Test 15 minutes over all time for flow unit.
-				mocha.it('C1: 1 day shift end 2022-10-31 17:00:00 for 15 minute reading intervals and quantity units & kWh as kWh', async () => {
-					await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
-					// Get the unit ID since the DB could use any value.
-					const unitId = await getUnitId('kWh');
-					const expected = [3120.01835362067, 3367.50141893133];
-					// for compare, need the unitID, currentStart, currentEnd, shift
-					const res = await chai.request(app).get(`/api/compareReadings/meters/${METER_ID}`)
-						.query({
-							curr_start: '2022-10-31 00:00:00',
-							curr_end: '2022-10-31 17:00:00',
-							shift: 'P1D',
-							graphicUnitId: unitId
-						});
-					expectCompareToEqualExpected(res, expected);
+mocha.describe("readings API", () => {
+	mocha.describe(
+		"readings test, test if data returned by API is as expected",
+		() => {
+			mocha.describe("for compare charts", () => {
+				mocha.describe("for meters", () => {
+					// Test 15 minutes over all time for flow unit.
+					mocha.it(
+						"C1: 1 day shift end 2022-10-31 17:00:00 for 15 minute reading intervals and quantity units & kWh as kWh",
+						async () => {
+							await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
+							// Get the unit ID since the DB could use any value.
+							const unitId = await getUnitId("kWh");
+							const expected = [3120.01835362067, 3367.50141893133];
+							// for compare, need the unitID, currentStart, currentEnd, shift
+							const res = await chai
+								.request(app)
+								.get(`/api/compareReadings/meters/${METER_ID}`)
+								.query({
+									curr_start: "2022-10-31 00:00:00",
+									curr_end: "2022-10-31 17:00:00",
+									shift: "P1D",
+									graphicUnitId: unitId,
+								});
+							expectCompareToEqualExpected(res, expected);
+						}
+					);
+
+					mocha.it(
+						"C2: 7 day shift end 2022-10-31 17:00:00 for 15 minute reading intervals and quantity units & kWh as kWh",
+						async () => {
+							await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
+							// Get the unit ID since the DB could use any value.
+							const unitId = await getUnitId("kWh");
+							const expected = [7962.23097109771, 8230.447588312];
+							// for compare, need the unitID, currentStart, currentEnd, shift
+							const res = await chai
+								.request(app)
+								.get(`/api/compareReadings/meters/${METER_ID}`)
+								.query({
+									curr_start: "2022-10-30 00:00:00",
+									curr_end: "2022-10-31 17:00:00",
+									shift: "P7D",
+									graphicUnitId: unitId,
+								});
+							expectCompareToEqualExpected(res, expected);
+						}
+					);
+
+					mocha.it(
+						"C3: 28 day shift end 2022-10-31 17:00:00 for 15 minute reading intervals and quantity units & kWh as kWh",
+						async () => {
+							await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
+							// Get the unit ID since the DB could use any value.
+							const unitId = await getUnitId("kWh");
+							const expected = [108269.924822581, 108889.847659507];
+							// for compare, need the unitID, currentStart, currentEnd, shift
+							const res = await chai
+								.request(app)
+								.get(`/api/compareReadings/meters/${METER_ID}`)
+								.query({
+									curr_start: "2022-10-09 00:00:00",
+									curr_end: "2022-10-31 17:00:00",
+									shift: "P28D",
+									graphicUnitId: unitId,
+								});
+							expectCompareToEqualExpected(res, expected);
+						}
+					);
+					// Add C4 here
+
+					// Add C5 here
+
+					// Add C6 here
+
+					// Add C8 here
+
+					// Add C9 here
+
+					// Add C10 here
+
+					// Add C11 here
+
+					// Add C12 here
+
+					// Add C13 here
+
+					// Add C14 here
+					mocha.it(
+						"C14: 1 day shift end 2022-10-31 17:00:00 for 15 minute reading intervals and quantity units & kWh as lbs of CO2 & chained & reversed",
+						async () => {
+							const unitData = [
+								{
+									// u2
+									name: "Electric_Utility",
+									identifier: "",
+									unitRepresent: Unit.unitRepresentType.QUANTITY,
+									secInRate: 3600,
+									typeOfUnit: Unit.unitType.METER,
+									suffix: "",
+									displayable: Unit.displayableType.NONE,
+									preferredDisplay: false,
+									note: "special unit",
+								},
+
+								{
+									// u10
+									name: "kg",
+									identifier: "",
+									unitRepresent: Unit.unitRepresentType.QUANTITY,
+									secInRate: 3600,
+									typeOfUnit: Unit.unitType.UNIT,
+									suffix: "",
+									displayable: Unit.displayableType.ALL,
+									preferredDisplay: false,
+									note: "OED created standard unit",
+								},
+
+								{
+									// u11
+									name: "metric ton",
+									identifier: "",
+									unitRepresent: Unit.unitRepresentType.QUANTITY,
+									secInRate: 3600,
+									typeOfUnit: Unit.unitType.UNIT,
+									suffix: "",
+									displayable: Unit.displayableType.ALL,
+									preferredDisplay: false,
+									note: "OED created standard unit",
+								},
+
+								{
+									// u12
+									name: "kg CO₂",
+									identifier: "",
+									unitRepresent: Unit.unitRepresentType.QUANTITY,
+									secInRate: 3600,
+									typeOfUnit: Unit.unitType.UNIT,
+									suffix: "CO₂",
+									displayable: Unit.displayableType.ALL,
+									preferredDisplay: false,
+									note: "special unit",
+								},
+
+								{
+									// u13
+									name: "pound",
+									identifier: "lb",
+									unitRepresent: Unit.unitRepresentType.QUANTITY,
+									secInRate: 3600,
+									typeOfUnit: Unit.unitType.UNIT,
+									suffix: "",
+									displayable: Unit.displayableType.ALL,
+									preferredDisplay: false,
+									note: "special unit",
+								},
+							];
+
+							const conversionData = [
+								// c11
+								{
+									sourceName: "Electric_Utility",
+									destinationName: "kg CO₂",
+									bidirectional: false,
+									slope: 0.709,
+									intercept: 0,
+									note: "Electric_Utility → kg CO₂",
+								},
+								// c12
+								{
+									sourceName: "kg CO₂",
+									destinationName: "kg",
+									bidirectional: false,
+									slope: 1,
+									intercept: 0,
+									note: "CO₂ → kg",
+								},
+								// c13
+								{
+									sourceName: "kg",
+									destinationName: "metric ton",
+									bidirectional: true,
+									slope: 1e-3,
+									intercept: 0,
+									note: "kg → Metric ton",
+								},
+								// c14
+								{
+									sourceName: "pound",
+									destinationName: "metric ton",
+									bidirectional: true,
+									slope: 454.545454,
+									intercept: 0,
+									note: "lbs → metric tons",
+								},
+							];
+						}
+					);
 				});
-
-				mocha.it('C2: 7 day shift end 2022-10-31 17:00:00 for 15 minute reading intervals and quantity units & kWh as kWh', async () => {
-					await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
-					// Get the unit ID since the DB could use any value.
-					const unitId = await getUnitId('kWh');
-					const expected = [7962.23097109771, 8230.447588312];
-					// for compare, need the unitID, currentStart, currentEnd, shift
-					const res = await chai.request(app).get(`/api/compareReadings/meters/${METER_ID}`)
-						.query({
-							curr_start: '2022-10-30 00:00:00',
-							curr_end: '2022-10-31 17:00:00',
-							shift: 'P7D',
-							graphicUnitId: unitId
-						});
-					expectCompareToEqualExpected(res, expected);
-				});
-
-				mocha.it('C3: 28 day shift end 2022-10-31 17:00:00 for 15 minute reading intervals and quantity units & kWh as kWh', async () => {
-					await prepareTest(unitDatakWh, conversionDatakWh, meterDatakWh);
-					// Get the unit ID since the DB could use any value.
-					const unitId = await getUnitId('kWh');
-					const expected = [108269.924822581, 108889.847659507];
-					// for compare, need the unitID, currentStart, currentEnd, shift
-					const res = await chai.request(app).get(`/api/compareReadings/meters/${METER_ID}`)
-						.query({
-							curr_start: '2022-10-09 00:00:00',
-							curr_end: '2022-10-31 17:00:00',
-							shift: 'P28D',
-							graphicUnitId: unitId
-						});
-					expectCompareToEqualExpected(res, expected);
-				});
-				// Add C4 here
-
-				// Add C5 here
-
-				// Add C6 here
-
-				// Add C8 here
-
-				// Add C9 here
-
-				// Add C10 here
-
-				// Add C11 here
-
-				// Add C12 here
-
-				// Add C13 here
-
-				// Add C14 here
 			});
-		});
-	});
+		}
+	);
 });
