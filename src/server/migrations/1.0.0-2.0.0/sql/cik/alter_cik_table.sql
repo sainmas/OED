ccd8c65c9cf00b52643e4ae821c140c4ab92c1e7
Steven Huss-Lederman <huss@beloit.edu>
2024-11-18 17:07:33 -0600

    Fix up comment

--- src/server/migrations/1.0.0-2.0.0/sql/cik/alter_cik_table.sql
+++ src/server/migrations/1.0.0-2.0.0/sql/cik/alter_cik_table.sql
@@ -12,3 +12,6 @@ CREATE TABLE IF NOT EXISTS cik (
 	intercept FLOAT,
 	PRIMARY KEY (source_id, destination_id)
 );
+
+-- TODO It is important that a redoCik is done after this to recreate cik.
+-- MUST ADD THIS TO THE 2.0 MIGRATION.
