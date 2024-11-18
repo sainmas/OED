31da7f15ba4d02f40ca7e6d2262a04adb370a2b7
Eboyd3443 <ETBoyd@proton.me>
2024-11-18 13:46:06 -0800

    Add first mocha test case for Meter.type

--- src/server/test/db/enumTests.js
+++ src/server/test/db/enumTests.js
@@ -1,24 +1,33 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
- 
+
 const Meter = require('../../models/Meter');
 const Database = require('../../models/database');
 const DB = require('../../db');
+const { expectArrayOfUnitsToBeEquivalent } = require('../../util/compareUnits');
+const { mocha, expect } = require('../common');
 
-try {
-    const conn = DB.getConnection();
-    conn.result(Database.sqlFile('meter/get_meter_enum.sql')).then(data => {
-        let resultArray = data.rows;
-        resultArray.forEach((item) =>{
-            console.log(item.unnest);
-        });
-        //console.log(resultArray.length);
-    }).catch(err => {
-            console.log(err);
-        }
-    );
-}
-catch(err) {
-    console.log.error(`Error while performing database test ${err}`, err);
-}
+mocha.describe('Enums JS to DB', () => {
+	mocha.it('can equate Meter.type to SQL enum', async () => {
+        const conn = DB.getConnection();
+        let serverEnum = [];
+        let jsObject = [];
+        conn.result(Database.sqlFile('meter/get_meter_enum.sql')).then(data => {
+            let resultArray = data.rows;
+            resultArray.forEach((item) =>{
+                serverEnum.push(item.unnest);
+            });
+            for(let key in Meter.type) {
+                if (Meter.type.hasOwnProperty(key)) {
+                    let value = Meter.type[key];
+                    jsObject.push(value);
+                }
+            }
+            serverEnum.sort();
+            jsObject.sort();
+            expect(serverEnum.toString()).to.equal(jsObject.toString());
+            expect(serverEnum.length).to.equal(jsObject.length);
+        })
+    })
+})
